01894d91474a9abae52c36baa34b6aa1cf360abc
alvaro <abv0013@alu.medac.es>
2023-11-17 09:39:48 +0100

    versión inicial

---
+++ app/Views/content/coches.php
@@ -0,0 +1,52 @@
+    <div class="col-9"> 
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6 col-md-6 bg-primary">
+                    Hola
+                </div>
+                <div class="col-6 col-md-3 bg-warning">
+                    Hola
+                </div>
+                <div class="col-6 col-md-2 bg-success">
+                    Hola
+                </div>
+                <div class="col-6 col-md-1 bg-danger">
+                    Hola
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-md-center">
+            <div class="col-12 col-md-4 m-0 p-4";>
+                <div class="card shadow-lg">
+                    <img src="https://lonelyplanetes.cdnstatics2.com/sites/default/files/styles/max_1300x1300/public/fotos/japon_tokio_montefuji_shutterstock_756354574_sean_pavone_shutterstock.jpg?itok=3nrPOQvT" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">TOKIO</h4>
+                        <p class="card-text">Viajar a Tokio</p>
+                        <a href="https://www.iberia.com/flights/?market=ES&language=es&appliesOMB=false&splitEndCity=false&initializedOMB=true&flexible=true&TRIP_TYPE=2&BEGIN_CITY_01=MAD&END_CITY_01=TYO&nombreOrigen=Madrid&nombreDestino=Tokio&BEGIN_DAY_01=01&BEGIN_MONTH_01=202312&BEGIN_YEAR_01=2023&END_DAY_01=31&END_MONTH_01=202312&END_YEAR_01=2023&FARE_TYPE=R&quadrigam=IBHMPA&ADT=1&CHD=0&INF=0&BNN=0&YTH=0&YCD=0&residentCode=&familianumerosa=&BV_UseBVCookie=no&boton=Buscar&bookingMarket=ES#!/availability" class="btn btn-primary">Comprar billete</a>
+                    </div> 
+                </div>
+            </div>
+            <div class="col-12 col-md-4 m-0 p-4";>
+                <div class="card shadow-lg">
+                    <img src="https://lonelyplanetes.cdnstatics2.com/sites/default/files/styles/max_1300x1300/public/fotos/japon_tokio_montefuji_shutterstock_756354574_sean_pavone_shutterstock.jpg?itok=3nrPOQvT" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">TOKIO</h4>
+                        <p class="card-text">Viajar a Tokio</p>
+                        <a href="https://www.iberia.com/flights/?market=ES&language=es&appliesOMB=false&splitEndCity=false&initializedOMB=true&flexible=true&TRIP_TYPE=2&BEGIN_CITY_01=MAD&END_CITY_01=TYO&nombreOrigen=Madrid&nombreDestino=Tokio&BEGIN_DAY_01=01&BEGIN_MONTH_01=202312&BEGIN_YEAR_01=2023&END_DAY_01=31&END_MONTH_01=202312&END_YEAR_01=2023&FARE_TYPE=R&quadrigam=IBHMPA&ADT=1&CHD=0&INF=0&BNN=0&YTH=0&YCD=0&residentCode=&familianumerosa=&BV_UseBVCookie=no&boton=Buscar&bookingMarket=ES#!/availability" class="btn btn-primary">Comprar billete</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 m-0 p-4";>
+                <div class="card shadow-lg">
+                    <img src="https://lonelyplanetes.cdnstatics2.com/sites/default/files/styles/max_1300x1300/public/fotos/japon_tokio_montefuji_shutterstock_756354574_sean_pavone_shutterstock.jpg?itok=3nrPOQvT" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">TOKIO</h4>
+                        <p class="card-text">Viajar a Tokio</p>
+                        <a href="https://www.iberia.com/flights/?market=ES&language=es&appliesOMB=false&splitEndCity=false&initializedOMB=true&flexible=true&TRIP_TYPE=2&BEGIN_CITY_01=MAD&END_CITY_01=TYO&nombreOrigen=Madrid&nombreDestino=Tokio&BEGIN_DAY_01=01&BEGIN_MONTH_01=202312&BEGIN_YEAR_01=2023&END_DAY_01=31&END_MONTH_01=202312&END_YEAR_01=2023&FARE_TYPE=R&quadrigam=IBHMPA&ADT=1&CHD=0&INF=0&BNN=0&YTH=0&YCD=0&residentCode=&familianumerosa=&BV_UseBVCookie=no&boton=Buscar&bookingMarket=ES#!/availability" class="btn btn-primary">Comprar billete</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
